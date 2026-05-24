442874e20913f9868808dc1e06205f63d5e14069
Evgeny Solovev <solovev257@gmail.com>
2026-05-24 14:22:34 +0400

    Добавлена валидация email в класс User

--- src/models/user.py
+++ src/models/user.py
@@ -5,3 +5,8 @@ class User:
     
     def get_info(self):
         return "Пользователь: " + self.name + ", Email: " + self.email
+
+def validate_email(self):
+    if "@" not in self.email:
+        raise ValueError("Неверный формат email")
+    return True
