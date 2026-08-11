b43d08888fd3443982bcd9b57d4f91c9429def11
Evgeny Solovev <solovev257@gmail.com>
2026-08-11 20:15:33 +0400

    Решение проблемы ромбовидного наследования

--- src/models/payment.py
+++ src/models/payment.py
@@ -1,24 +1,50 @@
-class Payment:
-    def __init__(self, amount):
-        self.amount = amount
-    
-    def process_payment(self):
-        raise NotImplementedError("Метод должен быть переопределен в дочернем классе")
+# class Payment:
+#     def __init__(self, amount):
+#         self.amount = amount
+#
+#     def process_payment(self):
+#         raise NotImplementedError("Метод должен быть переопределен в дочернем классе")
+#
+# class CardPayment(Payment):
+#     def __init__(self, amount, card_number):
+#         super().__init__(amount)
+#         self.__card_number = card_number  # Приватный атрибут
+#
+#     def process_payment(self):
+#         # Маскируем номер карты для безопасности
+#         masked_card = "**** " + self.__card_number[-4:]
+#         return "Оплата картой " + masked_card + ": " + str(self.amount) + " руб."
+#
+# class PayPalPayment(Payment):
+#     def __init__(self, amount, email):
+#         super().__init__(amount)
+#         self.email = email
+#
+#     def process_payment(self):
+#         return "Оплата PayPal (" + self.email + "): " + str(self.amount) + " руб."
 
-class CardPayment(Payment):
-    def __init__(self, amount, card_number):
-        super().__init__(amount)
-        self.__card_number = card_number  # Приватный атрибут
-    
-    def process_payment(self):
-        # Маскируем номер карты для безопасности
-        masked_card = "**** " + self.__card_number[-4:]
-        return "Оплата картой " + masked_card + ": " + str(self.amount) + " руб."
+class A:
+    def method(self):
+        print("A.method()")
 
-class PayPalPayment(Payment):
-    def __init__(self, amount, email):
-        super().__init__(amount)
-        self.email = email
-    
-    def process_payment(self):
-        return "Оплата PayPal (" + self.email + "): " + str(self.amount) + " руб."
+class B(A):
+    def method(self):
+        print("B.method()")
+        super().method()
+
+class C(A):
+    def method(self):
+        print("C.method()")
+        super().method()
+
+class D(B,C):
+    def method(self):
+        print("D.method()")
+        super().method()
+
+print("MRO для D")
+for i, cls in enumerate(D.mro(), 1):
+    print(f"{i}. {cls.__name__}")
+
+d = D()
+d.method()
